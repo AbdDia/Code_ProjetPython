8edad41802de06a55aba86a2437b1e835a7bbb91
Valinquish <13287345+Valinquish@users.noreply.github.com>
2021-01-10 04:09:02 +0100

    Major update
    - splitted functions into multiple files to makes code simpler
    - you can now call HTML or PDF objects for your reporting
    - stepdisc rework (simplification)
    - suppression of olds/unnecessary files

--- demo_program.py
+++ demo_program.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 28 19:03:20 2020
-
-@author: Valinquish
+Démonstration de l'application d'Analyse Discriminante Linéaire
 """
 
 import pandas as pd
-#import stepdisc as s
-from LDA_v2 import LinearDiscriminantAnalysis as LDA
-
+from discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from reporting import HTML, PDF
 
 # test avec bdd normale
-DTrain = pd.read_excel("Data_Illustration_Livre_ADL.xlsx",
+DTrain = pd.read_excel("data\Data_Illustration_Livre_ADL.xlsx",
                        sheet_name="DATA_2_TRAIN", engine="openpyxl")
 
 # création d'un objet LDA permettant de faire une analyse discriminante 
@@ -21,20 +18,24 @@ lda = LDA(DTrain, "TYPE")
 # PROC DISCRIM
 lda.fit()
 print(lda.intercept_, lda.coef_)
-lda.discrim_html_output() # sortie html
-#lda.discrim_pdf_output() # sortie pdf
+# REPORTING
+HTML().discrim_html_output(lda, "output\discrim_results.html") # sortie html
+PDF().discrim_pdf_output(lda, "output\discrim_results.pdf") # sortie pdf
 
 # PROC STEPDISC
 lda.wilks_decay()
 lda.stepdisc(0.01, "forward")
+HTML().stepdisc_html_output(lda, "output\stepdisc_forward_results.html")
 lda.stepdisc(0.01, "backward")
+HTML().stepdisc_html_output(lda, "output\stepdisc_backward_results.html")
 
 # PREDICTION
-DP = pd.read_excel("Data_Illustration_Livre_ADL.xlsx",sheet_name="DATA_2_TEST")
+DP = pd.read_excel("data\Data_Illustration_Livre_ADL.xlsx",
+                   sheet_name="DATA_2_TEST")
 y_true = DP.TYPE
 valeurs_a_predire = DP[DP.columns[1:]]
 y_pred = lda.predict(valeurs_a_predire)
-lda.confusion_matrix(y_true,y_pred)
+lda.confusion_matrix(y_true, y_pred)
 lda.accuracy_score(y_true, y_pred)
 print('Confusion matrix: ', lda.confusionMatrix)
 print('Accuracy score: ', lda.accuracy)
