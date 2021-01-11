5bcd828f5e07cce4b8267f4b68186f0cbaf228eb
Valinquish <13287345+Valinquish@users.noreply.github.com>
2021-01-11 19:47:30 +0100

    update demo_program + rapport

--- demo_program.py
+++ demo_program.py
@@ -7,7 +7,7 @@ import pandas as pd
 from discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from reporting import HTML, PDF
 
-# test avec bdd normale
+# Chargement des données
 DTrain = pd.read_excel("data\Data_Illustration_Livre_ADL.xlsx",
                        sheet_name="DATA_2_TRAIN", engine="openpyxl")
 
@@ -20,11 +20,11 @@ help(lda)
 dir(lda)
 lda.classNames
 
-# apprentissage, ~PROC DISCRIM de SAS
+#---- Apprentissage, ~PROC DISCRIM de SAS
 lda.fit()
 print(lda.intercept_, '\n',lda.coef_)
 
-# PREDICTION
+# Prédiction
 DP = pd.read_excel("data\Data_Illustration_Livre_ADL.xlsx",
                    sheet_name="DATA_2_TEST")
 
@@ -32,7 +32,7 @@ valeurs_a_predire = DP[DP.columns[1:]]
 y_pred = lda.predict(valeurs_a_predire)
 print(y_pred)
 
-# REPORTING
+# Reporting PROC DISCRIM
 HTML().discrim_html_output(lda, "output\discrim_results.html") # sortie html
 PDF().discrim_pdf_output(lda, "output\discrim_results.pdf") # sortie pdf
 
@@ -43,7 +43,7 @@ lda.accuracy_score(y_true, y_pred)
 print('Confusion matrix: ', lda.confusionMatrix)
 print('Accuracy score: ', lda.accuracy)
 
-# PROC STEPDISC
+#---- Sélection de variables, ~PROC STEPDISC de SAS
 # Approche ascendante
 lda.stepdisc(0.01, "forward")
 HTML().stepdisc_html_output(lda, "output\stepdisc_forward_results.html")
@@ -52,10 +52,18 @@ lda.stepdisc(0.01, "backward")
 HTML().stepdisc_html_output(lda, "output\stepdisc_backward_results.html")
 
 # Décroissance du lambda de Wilks
+# chargement des données
 DTrainLarge = pd.read_excel("data\Data_Illustration_Livre_ADL.xlsx",
                             sheet_name="WAVE_NOISE", engine="openpyxl")
-lda_wave_noise = LDA(DTrainLarge, "classe")
+lda_wave_noise = LDA(DTrainLarge, "classe") # création de l'objet
+# Appel de la fonction
 lda_wave_noise.wilks_decay()
+# Affichage des valeurs du lambda
 print("Valeurs du lambda de Wilks:", lda_wave_noise.infoWilksDecay)
+# Affichage de la courbe
 lda_wave_noise.figWilksDecay
 lda_wave_noise.figWilksDecay.savefig("output\décroissance_de_wilks.png",dpi=200)
+
+# vous pouvez également faire de la sélection de variables sur ce jeu de données ! ;)
+lda_wave_noise.stepdisc(0.01, "forward")
+HTML().stepdisc_html_output(lda_wave_noise, "output\stepdisc_wave_noise")
